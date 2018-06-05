bc007340c49ab89b8dee02c5fe772bcaec1bcd12
Clary <dianaclarissa982@gmail.com>
2018-06-05 09:09:23 +0200

    Preferences

--- app/src/main/java/com/example/android/newsapp1/MainActivity.java
+++ app/src/main/java/com/example/android/newsapp1/MainActivity.java
@@ -4,12 +4,16 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -62,7 +66,22 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
-        return new NewsLoader(this, NEWS_URL);
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+
+        String orderBy = sharedPref.getString(getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default));
+
+//*https://content.guardianapis.com/search?show-fields=byline&section=books&api-key=test*//
+
+        Uri baseUri = Uri.parse(NEWS_URL);
+        Uri.Builder uribuilder = baseUri.buildUpon();
+        uribuilder.appendQueryParameter("show-fields", "section");
+        uribuilder.appendQueryParameter("byline", orderBy);
+        uribuilder.appendQueryParameter("api-key", "test");
+
+
+        return new NewsLoader(this, uribuilder.toString());
     }
 
     @Override
@@ -80,5 +99,22 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onLoaderReset(Loader<List<News>> loader) {
         mAdapter.clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id== R.id.action_settings){
+            Intent settingsIntent = new Intent(this, NewsSettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
